8296cf567c93bd56c09c865b7c1a20a4d1c62c5b
Nikolya777777777777 <105540373+Nikolya777777777777@users.noreply.github.com>
2024-01-31 12:29:06 +0100

    Create Lr12Kovb.java
    This is the second class

---
+++ Lr12Kovb.java
@@ -0,0 +1,16 @@
+public class Lr12Kovb {
+
+    public static void main(String[] args) {
+        Flower flower = new Flower("Red");
+
+        flower.fadeAway();
+        flower.toBlossom();
+
+        Bud bud = new Bud("Yellow");
+
+        bud.printColor();
+
+
+    }
+
+}
